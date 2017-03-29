83c83b1f197398971001732bd8733809f2a99f79
fbk-rena <fbk.rena@gmail.com>
2017-03-29 11:49:48 -0600

    Funcion agregar listas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-alert("Hola")
+//alert("Hola")
+
+function crearLista(){
+
+	var nuevaLista = document.createElement("ul");
+	var nombreIngresado = document.getElementById("nombreLista").value;
+	var nombreLista= document.createTextNode(nombreIngresado);
+
+	nuevaLista.appendChild(nombreLista);
+
+	var listas= document.getElementById("cajaListas");
+
+	listas.appendChild(nuevaLista);
+
+	var addTarea= document.createElement("h5");
+	var nombreAdd= document.createTextNode("Agrega tareas a esta lista");
+
+	addTarea.appendChild(nombreAdd);
+
+	var botonTareas = document.createElement("button");
+	var nombreboton = document.createTextNode("Agregar");
+
+	botonTareas.appendChild(nombreboton);
+	
+
+	var ingresTarea = document.createElement("input");
+	
+
+	ingresTarea.appendChild(addTarea);
+
+	nuevaLista.appendChild(addTarea);
+	nuevaLista.appendChild(ingresTarea);
+	nuevaLista.appendChild(botonTareas);
+
+	nombreIngresado= "";
+
+	console.log(listas);
+}
